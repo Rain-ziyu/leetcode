860e4f7b09d79c0da5baacfce143dccc77a8118f
wanwenlong <wanwenlong@xinanli.com>
2023-11-08 09:36:55 +0800

    feat(LeetCode): 最长平衡子串dp慢写法，其实使用一维数组即可，最优解直接统计连续的0然后连续的1取其中最小值的2倍即可
    https://leetcode.cn/problems/find-the-longest-balanced-substring-of-a-binary-string/solutions/2204127/onfu-za-du-o1kong-jian-de-jie-fa-by-cute-t3er/?envType=daily-question&envId=2023-11-08

---
+++ src/LongestBalancedSubstring.go
@@ -0,0 +1,39 @@
+package main
+
+func main() {
+	println(findTheLongestBalancedSubstring("01011"))
+}
+
+// @Method: findTheLongestBalancedSubstring
+// @Description: 最长平衡子字符串
+// @param s
+// @return int
+func findTheLongestBalancedSubstring(s string) int {
+	dp := make([][]int, len(s))
+	res := 0
+	//	初始化dp数组
+	for i := 0; i < len(s); i++ {
+		// 其实对于dp数组来说只需要对角线下面的一半即可
+		dp[i] = make([]int, i)
+		//	初始化dp数组，其中满足字符串中[i][i-1]字符相等时，dp[i][i-1] = 1
+		if i > 0 && s[i] == '1' && s[i-1] == '0' {
+			dp[i][i-1] = 2
+			res = 2
+		}
+	}
+
+	//	横向遍历dp数组 状态转移方程为 f(m,n) = f(m-1,n+1) + 1 且 m-n > 1 且 m-n+1一定是偶数
+	for i := 2; i < len(s); i++ {
+		j := 0
+		if i%2 == 0 {
+			j = 1
+		}
+		for ; j < i-1; j = j + 2 {
+			if s[i] == '1' && s[j] == '0' && (j-i+1)%2 == 0 && dp[i-1][j+1] > 0 {
+				dp[i][j] = dp[i-1][j+1] + 2
+				res = max(res, dp[i][j])
+			}
+		}
+	}
+	return res
+}
